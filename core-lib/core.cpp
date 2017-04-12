e7ccbbddc80450c635f11f01d48c5919340cac86
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2017-04-12 16:27:40 +0200

    added initial core project..

---
+++ core-lib/core.cpp
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   core.cpp
+ * Author: Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
+ * 
+ * Created on April 12, 2017, 4:25 PM
+ */
+
+#include "core.h"
+
+namespace ratio {
+
+    core::core() { }
+
+    core::~core() { }
+}
---
+++ core-lib/core.h
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   core.h
+ * Author: Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
+ *
+ * Created on April 12, 2017, 4:25 PM
+ */
+
+#ifndef CORE_H
+#define CORE_H
+
+namespace ratio {
+
+    class core {
+    public:
+        core();
+        core(const core& orig) = delete;
+        virtual ~core();
+    private:
+
+    };
+}
+
+#endif /* CORE_H */
+
