589b1de61c581edfd3feacca826e230294e16480
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2017-05-17 17:25:21 +0200

    solved minor issues..

--- translators/pddl2ratio/src/it/cnr/istc/translators/pddl2ratio/parser/Translator.java
+++ translators/pddl2ratio/src/it/cnr/istc/translators/pddl2ratio/parser/Translator.java
@@ -29,13 +29,20 @@ import it.cnr.istc.translators.pddl2ratio.language.Variable;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.BitSet;
 import java.util.Collections;
 import java.util.Set;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroupFile;
@@ -46,6 +53,7 @@ import org.stringtemplate.v4.STGroupFile;
  */
 public class Translator {
 
+    private static final Logger LOG = Logger.getLogger(Translator.class.getName());
     private static final ParseTreeWalker WALKER = new ParseTreeWalker();
 
     public static PDDLInstance parse(File pddl_domain, File pddl_problem) throws IOException {
@@ -59,14 +67,48 @@ public class Translator {
         domain_lexer.requirements.addAll(domain_requirements);
         PDDLParser domain_parser = new PDDLParser(new CommonTokenStream(domain_lexer));
         domain_parser.requirements.addAll(domain_requirements);
-        domain_parser.addErrorListener(new BaseErrorListener());
+        domain_parser.addErrorListener(new ANTLRErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+                LOG.severe(msg);
+            }
+
+            @Override
+            public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact, BitSet ambigAlts, ATNConfigSet configs) {
+            }
+
+            @Override
+            public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex, BitSet conflictingAlts, ATNConfigSet configs) {
+            }
+
+            @Override
+            public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction, ATNConfigSet configs) {
+            }
+        });
 
         // we create the problem parser..
         PDDLLexer problem_lexer = new PDDLLexer(CharStreams.fromString(new String(Files.readAllBytes(pddl_problem.toPath())).toLowerCase()));
         problem_lexer.requirements.addAll(problem_requirements);
         PDDLParser problem_parser = new PDDLParser(new CommonTokenStream(problem_lexer));
         problem_parser.requirements.addAll(problem_requirements);
-        problem_parser.addErrorListener(new BaseErrorListener());
+        problem_parser.addErrorListener(new ANTLRErrorListener() {
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+                LOG.severe(msg);
+            }
+
+            @Override
+            public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact, BitSet ambigAlts, ATNConfigSet configs) {
+            }
+
+            @Override
+            public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex, BitSet conflictingAlts, ATNConfigSet configs) {
+            }
+
+            @Override
+            public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction, ATNConfigSet configs) {
+            }
+        });
 
         // We parse the domain..
         PDDLParser.DomainContext domain_context = domain_parser.domain();
@@ -81,20 +123,7 @@ public class Translator {
             /**
              * We define all the types of the domain..
              */
-            WALKER.walk(new PDDLBaseListener() {
-                @Override
-                public void enterTyped_list_name(PDDLParser.Typed_list_nameContext ctx) {
-                    ctx.name().stream().forEach(type_name -> {
-                        Type type = new Type(Utils.capitalize(type_name.getText()));
-                        domain.addType(type);
-                    });
-                }
-            }, domain_context.types_def());
-
-            /**
-             * We set the superclasses (i.e., we manage type inheritance)..
-             */
-            WALKER.walk(new PDDLBaseListener() {
+            ParseTreeWalker.DEFAULT.walk(new PDDLBaseListener() {
                 @Override
                 public void enterTyped_list_name(PDDLParser.Typed_list_nameContext ctx) {
                     Type c_superclass = null;
@@ -102,12 +131,17 @@ public class Translator {
                         c_superclass = Type.OBJECT;
                     } else if (ctx.type().primitive_type().size() == 1) {
                         c_superclass = ctx.type().primitive_type(0).name() == null ? Type.OBJECT : domain.getType(Utils.capitalize(ctx.type().primitive_type(0).name().getText()));
+                        if (c_superclass == null) {
+                            c_superclass = new Type(Utils.capitalize(ctx.type().primitive_type(0).name().getText()));
+                            domain.addType(c_superclass);
+                        }
                     } else {
-                        throw new AssertionError("multiple inheritance..");
+                        c_superclass = new EitherType(ctx.type().primitive_type().stream().map(primitive_type -> primitive_type.name() == null ? Type.OBJECT : domain.getType(Utils.capitalize(primitive_type.name().getText()))).collect(Collectors.toList()));
+                        domain.addType(c_superclass);
                     }
                     final Type superclass = c_superclass;
-                    ctx.name().stream().forEach(type_name -> {
-                        Type type = domain.getType(Utils.capitalize(type_name.getText()));
+                    ctx.name().forEach(type_name -> {
+                        Type type = new Type(Utils.capitalize(type_name.getText()));
                         type.setSuperclass(superclass);
                         domain.addType(type);
                     });
@@ -119,7 +153,7 @@ public class Translator {
             /**
              * We define the constants.
              */
-            WALKER.walk(new PDDLBaseListener() {
+            ParseTreeWalker.DEFAULT.walk(new PDDLBaseListener() {
                 @Override
                 public void enterTyped_list_name(PDDLParser.Typed_list_nameContext ctx) {
                     Type type = null;
@@ -147,14 +181,14 @@ public class Translator {
             /**
              * We define the predicates.
              */
-            WALKER.walk(new PDDLBaseListener() {
+            ParseTreeWalker.DEFAULT.walk(new PDDLBaseListener() {
                 @Override
                 public void enterAtomic_formula_skeleton(PDDLParser.Atomic_formula_skeletonContext ctx) {
                     Variable[] variables = new Variable[0];
                     if (ctx.typed_list_variable() != null) {
                         // The predicate formula has parameters
                         TypedListVariableListener typedListVariable = new TypedListVariableListener(domain);
-                        WALKER.walk(typedListVariable, ctx.typed_list_variable());
+                        ParseTreeWalker.DEFAULT.walk(typedListVariable, ctx.typed_list_variable());
                         variables = typedListVariable.variables.toArray(new Variable[typedListVariable.variables.size()]);
                     }
                     domain.addPredicate(new Predicate(Utils.capitalize(ctx.predicate().name().getText()), variables));
@@ -166,7 +200,7 @@ public class Translator {
             /**
              * We define the functions.
              */
-            WALKER.walk(new PDDLBaseListener() {
+            ParseTreeWalker.DEFAULT.walk(new PDDLBaseListener() {
                 private Type function_type = null;
 
                 @Override
@@ -189,7 +223,7 @@ public class Translator {
                     if (ctx.typed_list_variable() != null) {
                         // The predicate formula has parameters
                         TypedListVariableListener typedListVariable = new TypedListVariableListener(domain);
-                        WALKER.walk(typedListVariable, ctx.typed_list_variable());
+                        ParseTreeWalker.DEFAULT.walk(typedListVariable, ctx.typed_list_variable());
                         variables = typedListVariable.variables.toArray(new Variable[typedListVariable.variables.size()]);
                     }
                     domain.addFunction(new Function(Utils.capitalize(ctx.function_symbol().name().getText()), function_type, variables));
@@ -202,14 +236,14 @@ public class Translator {
          */
         domain_context.structure_def().stream().forEach(structure_def -> {
             if (structure_def.action_def() != null) {
-                WALKER.walk(new PDDLBaseListener() {
+                ParseTreeWalker.DEFAULT.walk(new PDDLBaseListener() {
                     @Override
                     public void enterAction_def(PDDLParser.Action_defContext ctx) {
                         Variable[] variables = new Variable[0];
                         if (ctx.typed_list_variable() != null) {
                             // The action has parameters
                             TypedListVariableListener typedListVariable = new TypedListVariableListener(domain);
-                            WALKER.walk(typedListVariable, ctx.typed_list_variable());
+                            ParseTreeWalker.DEFAULT.walk(typedListVariable, ctx.typed_list_variable());
                             variables = typedListVariable.variables.toArray(new Variable[typedListVariable.variables.size()]);
                         }
                         Action action = new Action(Utils.capitalize(ctx.action_symbol().name().getText()), variables);
@@ -225,14 +259,14 @@ public class Translator {
                     }
                 }, structure_def.action_def());
             } else if (structure_def.durative_action_def() != null) {
-                WALKER.walk(new PDDLBaseListener() {
+                ParseTreeWalker.DEFAULT.walk(new PDDLBaseListener() {
                     @Override
                     public void enterDurative_action_def(PDDLParser.Durative_action_defContext ctx) {
                         Variable[] variables = new Variable[0];
                         if (ctx.typed_list_variable() != null) {
                             // The durative action has parameters
                             TypedListVariableListener typedListVariable = new TypedListVariableListener(domain);
-                            WALKER.walk(typedListVariable, ctx.typed_list_variable());
+                            ParseTreeWalker.DEFAULT.walk(typedListVariable, ctx.typed_list_variable());
                             variables = typedListVariable.variables.toArray(new Variable[typedListVariable.variables.size()]);
                         }
                         DurativeAction action = new DurativeAction(Utils.capitalize(ctx.da_symbol().name().getText()), variables);
@@ -256,7 +290,7 @@ public class Translator {
          * We define the objects.
          */
         if (problem_context.object_declaration() != null) {
-            WALKER.walk(new PDDLBaseListener() {
+            ParseTreeWalker.DEFAULT.walk(new PDDLBaseListener() {
                 @Override
                 public void enterTyped_list_name(PDDLParser.Typed_list_nameContext ctx) {
                     Type type = null;
